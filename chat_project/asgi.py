61656019efc59b239fcdd2029de3488e0fb28e5d
amir-rz <mr.amir.rezazadehh@gmail.com>
2021-03-27 11:40:00 +0430

    Added the channels layer and consumers functionality

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -1,14 +1,20 @@
-# mysite/asgi.py
+# chat_project/asgi.py
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 import django
 from channels.http import AsgiHandler
-from channels.routing import ProtocolTypeRouter
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
 django.setup()
 
 application = ProtocolTypeRouter({
     "http": AsgiHandler(),
-    # Just HTTP for now. (We can add other protocols later.)
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
 })
