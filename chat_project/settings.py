d08edf924bd7829e5deebe34bd376827eaa48c4a
amir-rz <mr.amir.rezazadehh@gmail.com>
2021-03-29 14:36:14 +0430

    requirements fixed

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -41,9 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-# Static folder path
-STATIC_URL = '/static/'
-
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -122,8 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Channels
 ASGI_APPLICATION = 'chat_project.asgi.application'
 CHANNEL_LAYERS = {
